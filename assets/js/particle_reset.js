7615701d7dfdbcf0129fc6627e1e821d34855466
Chirag Mahaveer Parmar <chiragparmar12209@gmail.com>
2018-07-28 16:48:07 +0530

    Blog Redesigned

--- assets/js/particle_reset.js
+++ assets/js/particle_reset.js
@@ -1,3 +1,10 @@
 var div_particle = document.getElementById("particles-js")
-div_particle.style.height = document.body.offsetHeight.toString() + "px"
+var assign_height = 0
+if (document.body.clientHeight > window.innerHeight){
+	assign_height = document.body.clientHeight
+}
+else{
+	assign_height = window.innerHeight
+}
+div_particle.style.height = assign_height.toString() + "px"
 console.log("height resetted")
