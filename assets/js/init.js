7693744ec50b84d2d72ddb70f2307d74c2142d70
chirag-parmar <chiragparmar12209@gmail.com>
2018-10-30 22:43:53 +0530

    particle-js on image load

--- assets/js/init.js
+++ assets/js/init.js
@@ -36,35 +36,61 @@ $(document).ready(function() {
     /**************************************************************************
              BLOG POST 
     **************************************************************************/
+    function dynamicallyLoadScript(url) {
+        var script = document.createElement("script"); //Make a script DOM node
+        script.src = url; //Set it's src to the provided URL
+        document.head.appendChild(script); //Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+    }
+
+    function resetHeight(){
+        var div_particle = document.getElementById("particles-js")
+        var assign_height = 0
+
+        if (document.body.clientHeight > window.innerHeight){
+            assign_height = document.body.clientHeight
+        }
+        else{
+            assign_height = window.innerHeight
+        }
+        div_particle.style.height = assign_height.toString() + "px"
+        
+        console.log("height resetted")
+    }
 
     var $masonry = $('.gallery-section');
     $masonry.masonry({
         itemSelector: '.gallery-item',
         columnWidth: '.gallery-item',
-        percentPosition: true
     });
     $masonry.imagesLoaded(function() {
         $masonry.masonry('layout');
+        resetHeight()
+        dynamicallyLoadScript("assets/js/particles.js")
+        dynamicallyLoadScript("assets/js/app.js")
     });
 
     var $masonry2 = $('.bloggy-section');
     $masonry2.masonry({
         itemSelector: '.blog-post',
         columnWidth: '.blog-post',
-        percentPosition: true
     });
     $masonry2.imagesLoaded(function() {
         $masonry2.masonry('layout');
+        resetHeight()
+        dynamicallyLoadScript("assets/js/particles.js")
+        dynamicallyLoadScript("assets/js/app.js")
     });
 
     var $masonry3 = $('.projjy-section');
     $masonry3.masonry({
         itemSelector: '.blog-post',
         columnWidth: '.blog-post',
-        percentPosition: true
     });
     $masonry3.imagesLoaded(function() {
         $masonry3.masonry('layout');
+        resetHeight()
+        dynamicallyLoadScript("assets/js/particles.js")
+        dynamicallyLoadScript("assets/js/app.js")
     });
 
 
--- assets/js/particle_reset.js
+++ assets/js/particle_reset.js
@@ -1,10 +1,22 @@
-var div_particle = document.getElementById("particles-js")
-var assign_height = 0
-if (document.body.clientHeight > window.innerHeight){
-	assign_height = document.body.clientHeight
+function dynamicallyLoadScript(url) {
+    var script = document.createElement("script"); //Make a script DOM node
+    script.src = url; //Set it's src to the provided URL
+    document.head.appendChild(script); //Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
 }
-else{
-	assign_height = window.innerHeight
+
+function resetHeight(){
+	var div_particle = document.getElementById("particles-js")
+	var assign_height = 0
+
+	if (document.body.clientHeight > window.innerHeight){
+		assign_height = document.body.clientHeight
+	}
+	else{
+		assign_height = window.innerHeight
+	}
+	div_particle.style.height = assign_height.toString() + "px"
+	
+	console.log("height resetted")
 }
-div_particle.style.height = assign_height.toString() + "px"
-console.log("height resetted")
+resetHeight()
+dynamicallyLoadScript("assets/js/particles.js")
